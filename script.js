d13e9ef84c5ff145e2c66921faa52cc393e80bd2
kolbeinnk <kok27@hi.is>
2018-10-30 21:01:40 +0000

    script.js

--- script.js
+++ script.js
@@ -13,7 +13,8 @@ const GAMES_TO_PLAY = 10;
  * Eftir leik er notanda boðið að spila annan leik, ef ekki hættir forrit.
  */
 function start() {
-  villa;
+  alert('Markmiðið er að svara eins mörgum af 10 dæmum rétt eins hratt og mögulegt er');
+  play();
 }
 
 /**
@@ -28,8 +29,25 @@ function start() {
  *
  */
 function play() {
-}
+  //Telja rétt svör og telja spurningar sem lagðar hafa verið fyrir
+  let correct = 0;
+  let question_count = 0;
+  const start = new Date();
 
+  do {
+    question_count+=1;
+    correct += ask();
+
+  } while(question_count < GAMES_TO_PLAY);
+
+  //Reikna tíma og meðalfjölda.
+  const end = new Date();
+  const time = (end - start)/1000;
+  const avg = (correct/time).toFixed(2);
+
+  alert('Þú svaraðir '  + correct + ' af 10 dæmum rétt á ' + time + ' sekúndum  \n Meðalrétt svör á sekúndu eru ' + avg);
+  quit();
+}
 /**
  * Spyr einnar spurningar og skilar upplýsingum um svar (mögulega með því að
  * nota true, false og null ef notandi hættir). Birtir notanda propmpt til að
@@ -45,6 +63,81 @@ function play() {
  * Sniðugt væri að færa það að búa til spurningu í nýtt fall sem ask() kallar í.
  */
 function ask() {
+
+  //Fáum fjögurra staka fylkið quest með öllu til að smíða spurninguna.
+  const quest = getQuestion();
+  rettSvar = quest.pop();
+  const svar = prompt('Hvað er ' + quest.pop() + quest.pop() + quest.pop());
+  // Ef ýtt var á cancel
+  if(svar===null){
+    alert('Hætt í leik');
+    quit();
+  }
+
+  const psvar = parseInt(svar);
+
+  if (psvar === rettSvar){
+    return 1;
+  }else{
+    return 0;
+  };
+}
+
+// Fall sem skilar fylki með tveimur tölum, tvíundarvirkja og rétta svarinu.
+function getQuestion(){
+    const teg = randomNumber(0,3);
+    //const teg = 3;
+    const numbers = [];
+
+//* - `+` dæmi þar sem báðar tölur geta verið á bilinu `[1, 100]`
+    if (teg===0) {
+      numbers.push(randomNumber(1,100));
+      numbers.push('+');
+      numbers.push(randomNumber(1,100));
+      numbers.push(numbers[0] + numbers[2]);
+      return numbers;
+    }
+
+//* `-` dæmi þar sem báðar tölur geta verið á bilinu `[1, 100]`
+    if (teg===1) {
+      numbers.push(randomNumber(1,100));
+      numbers.push('-');
+      numbers.push(randomNumber(1,100));
+      numbers.push(numbers[2] - numbers[0]);
+      return numbers;
+    }
+
+// `*` dæmi þar sem báðar tölur geta verið á bilinu `[1, 10]`
+  if (teg===2) {
+    numbers.push(randomNumber(1,10));
+    numbers.push('*');
+    numbers.push(randomNumber(1,10));
+    numbers.push(numbers[0] * numbers[2]);
+    return numbers;
+}
+//* - `/` dæmi þar sem fyrri tala er á bilinu `[2, 10]` og seinni talan er fyrri
+//*   talan sinnum tala á bilinu `[2, 10]` þ.a. svarið verði alltaf heiltala
+
+  if (teg===3) {
+    numbers.push(randomNumber(2,10));
+    numbers.push('/');
+    numbers.push(numbers[0]*randomNumber(1,10));
+    numbers.push(numbers[2] / numbers[0]);
+    return numbers;
+  }
+
+}
+
+//Hjálparfall til að byrja leikinn aftur ef hann klárast eða ýtt er á cancel.
+function quit(){
+  const answ =  confirm('Spila annan leik?');
+    if(answ == true){
+      play();
+    }
+    else {
+    //exit ekki til svo ég gef villu til að hætta keyrslu 
+     exit();
+    }
 }
 
 /**
